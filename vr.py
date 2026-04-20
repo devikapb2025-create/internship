f7fa1f253c10b834f824cd950af52d892c597ada
devikapb2025-create <devikapb2025@gmail.com>
2026-04-20 10:52:09 +0530

    Add files via upload

---
+++ vr.py
@@ -0,0 +1,151 @@
+#name1= "devika"
+#print(name1)
+#age= 19
+#print(age)
+#print(type(name1))
+#print(type(age))
+#print(float(age))
+#var1="46789"
+#print(float(var1))
+#var2=float(var1)
+#print(var2)
+#num1 =int(input("enter a number"))
+#if num1>0:
+#    print("positive")
+#elif num1 == 0:
+#    print("zero")
+#else:
+#    print("negative")
+#num2 = int(input("enter a number"))
+#if num2%2 ==0:
+#    print("even")
+#else:
+#    print("odd")
+#for i in range(1,51,3):
+#    print(i)
+#for i in range(1,21):
+#    if i % 2 == 0:
+#        print(i)
+#list1=["apple","cherry","mango"]
+#for i in list1:
+#    print(i)
+#print(list1[1])
+#print(list1[0:2])
+#n = 10
+#n9 = str(n)
+#print(n9)
+#print(type(n9))
+i = 1
+#while i < 10:
+#    print(i)
+#    if i == 5:
+#        break
+#    i += 1
+#while i < 10:
+#    if i == 5:
+#        continue
+#    print(i)
+#    i += 1
+#year = int(input("Enter year: "))
+#if (year % 4 == 0 and year % 100 != 0):
+#    print("Leap Year")
+#else:
+#    print("Not a Leap Year")
+#num = int(input("Enter number: "))
+#if num % 5 == 0 and num % 11 == 0:
+#    print("Divisible by both 5 and 11")
+#else:
+#    print("Not divisible by both 5 and 11")
+#for i in range(3, 31, 3):
+#    print(i)
+#i = 1
+#while i <= 100:
+#    print(i)
+#    i += 1
+#def add(a,b):
+#    c=a+b
+#    print("sum",c)
+#add(a=6,b=10)
+#def add(a,b):
+#    print("first name",a)
+#    print("last name",b)
+#add(a="devika",b="pb")
+#def display_info(first_name, last_name):
+#    print('first name:',fist_name)
+#    print('last name:',last_name)
+#display_info(last_name ='cartman')    
+#list1= ["apple","Orange","Mango"]
+#list1[1]="blackcurrant"
+#print(list1)
+#thistuple=("apple","banana","cherry")
+#y = list(thistuple)
+#y.remove("apple")
+#thistuple=tuple(y)
+#nums = [10, 15, 20, 25, 30]
+#print("Original list:", nums)
+
+# append
+#nums.append(35)
+#print("After append:", nums)
+
+# insert
+#nums.insert(2, 12)
+#print("After insert:", nums)
+
+# remove
+#nums.remove(15)
+#print("After remove:", nums)
+
+# pop
+#nums.pop()
+#print("After pop:", nums)
+
+# length
+#print("Length:", len(nums))
+#nums = [10, 15, 20, 25, 30]
+
+#print("Even numbers:")
+#for i in nums:
+#    if i % 2 == 0:
+#        print(i)
+#t = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+# convert to list
+#l = list(t)
+
+# change 3rd element (index 2)
+#l[2] = 100
+
+# convert back to tuple
+#t = tuple(l)
+
+#print(t)
+#def square(n):
+#    return n * n
+
+#print("Square:", square(5))
+#def student_details(name, age, course):
+#    print("Name:", name)
+#    print("Age:", age)
+#    print("Course:", course)
+
+#student_details(name="Shiya", age=18, course="BCA")
+#thisdict = {
+    #"brand": "ford",
+    #"model": "mustang",
+    #"year":1964
+#    }
+#print(thisdict)
+#dict = {
+#    "name":"devu",
+#    "age":19,
+#    "age":20,
+#    "college":"ymbc"
+#    }
+#print(dict)
+#x = dict.keys()
+#dict["name"]="pb"
+#print(dict)
+#dict.update({"age":25})
+#print(dict)
+
